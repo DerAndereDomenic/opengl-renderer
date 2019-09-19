15fcf213e8b737433534f724f4e23539ddb8fde4
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-09-19 19:44:08 +0200

    Add implementation of Vertex Array

---
+++ OpenGL Renderer/src/OpenGLObjects/impl/VertexArray.cpp
@@ -0,0 +1,42 @@
+#include <OpenGLObjects/VertexArray.h>
+
+VertexArray 
+VertexArray::createObject()
+{
+	VertexArray result;
+	glGenVertexArrays(1, &result._ID);
+	return result;
+}
+
+void 
+VertexArray::destroyObject(VertexArray &vao)
+{
+	glDeleteVertexArrays(1, &vao._ID);
+}
+
+void 
+VertexArray::bind()
+{
+	glBindVertexArray(_ID);
+}
+
+void 
+VertexArray::unbind()
+{
+	glBindVertexArray(0);
+}
+
+void 
+VertexArray::addBuffer(VertexBuffer &vbo, VertexBufferLayout &layout)
+{
+	vbo.bind();
+	bind();
+	unsigned int offset = 0;
+	for (unsigned int i = 0; i < layout.getElements().size(); ++i)
+	{
+		BufferElement element = layout.getElements()[i];
+		glEnableVertexAttribArray(i);
+		glVertexAttribPointer(i, element._count, element._type, element._normalized, layout.getVertexSize(), (const void*)offset);
+		offset += element._count * BufferElement::getSizeOfType(element._type);
+	}
+}
