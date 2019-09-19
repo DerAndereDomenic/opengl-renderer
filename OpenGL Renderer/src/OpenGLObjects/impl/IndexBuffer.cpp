92574060350c417ed2c051a8488d387cc6974ec3
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-09-19 18:44:11 +0200

    Generate a buffer

--- OpenGL Renderer/src/OpenGLObjects/impl/IndexBuffer.cpp
+++ OpenGL Renderer/src/OpenGLObjects/impl/IndexBuffer.cpp
@@ -8,7 +8,7 @@ IndexBuffer::createObject(unsigned int* data, unsigned int count)
 
 	result._count = count;
 
-	glGenBuffers(0, &result._ID);
+	glGenBuffers(1, &result._ID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, result._ID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*count, data, GL_STATIC_DRAW);
 
