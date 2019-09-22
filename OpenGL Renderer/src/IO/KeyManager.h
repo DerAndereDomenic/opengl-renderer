42edf9775ca9c203b0dfc32891a19aa9e9d7cc96
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-09-22 16:19:25 +0200

    Add a KeyManager Header

---
+++ OpenGL Renderer/src/IO/KeyManager.h
@@ -0,0 +1,49 @@
+#ifndef IO_KEY_MANAGER_H
+#define IO_KEY_MANAGER_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class KeyManager
+{
+public:
+	/**
+	*	\brief Instance of the KeyManager (Singleton)
+	*	\return A pointer to the KeyManager Instance
+	*/
+	static KeyManager* instance();
+
+	/**
+	*	\brief Setup the KeyManager
+	*	\param[in] window The window the keys should be registered in
+	*/
+	void setup(GLFWwindow* window);
+
+	/**
+	*	\brief Checks if a key is pressed
+	*	\param[in] key The key to be checked
+	*	\return True if the key is pressed, false otherwise
+	*/
+	bool isKeyDown(unsigned int key);
+
+	/**
+	*	\brief Check if the left mouse button is pressed
+	*	\return True if the key is pressed, false otherwise
+	*/
+	bool leftClicked();
+
+	/**
+	*	\brief Check if the right mouse button is pressed
+	*	\return True if the key is pressed, false otherwise
+	*/
+	bool rightClicked();
+
+private:
+	static KeyManager* _instance; /**<< The static KeyManager Pointer*/
+	KeyManager() = default; /**<< Make KeyManager private*/
+	KeyManager(const KeyManager&); /**<< Make KeyManager private*/
+
+	GLFWwindow* _window = nullptr; /**<< The Window of the KeyManager*/
+};
+
+#endif
