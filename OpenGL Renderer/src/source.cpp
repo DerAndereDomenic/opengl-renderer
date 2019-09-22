20af1555117cef2e29ee6b072e43d3f1a6559877
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-09-22 12:10:11 +0200

    Draw 2 elements with different vaos

--- OpenGL Renderer/src/source.cpp
+++ OpenGL Renderer/src/source.cpp
@@ -16,7 +16,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -28,25 +28,24 @@ int main(void)
 
 	if (glewInit() != GLEW_OK)
 		return -1;
-
 	Shader shader = Shader::createObject("C:/Users/DomenicZ/Documents/Visual Studio 2017/Projects/OpenGL Renderer/OpenGL Renderer/src/Shader/GLShaders/basic.vert",
 								         "C:/Users/DomenicZ/Documents/Visual Studio 2017/Projects/OpenGL Renderer/OpenGL Renderer/src/Shader/GLShaders/basic.frag");
+	shader.bind();
+	shader.setVec4("u_color", glm::vec4(0.3, 0.1, 0.4, 1));
+	shader.setInt("u_set", 0);
 
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-	0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-	-0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-	0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f
+	-0.75f, -0.25f, 0.0f, 1.0f,// 1.0f, 0.0f, 0.0f, 1.0f,
+	-0.25f, -0.25f, 0.0f, 1.0f,// 0.0f, 1.0f, 0.0f, 1.0f,
+	-0.75f, 0.25f, 0.0f, 1.0f,// 0.0f, 0.0f, 1.0f, 1.0f,
+	-0.25f, 0.25f, 0.0f, 1.0f,// 1.0f, 1.0f, 0.0f, 1.0f
 	};
 
-	VertexBuffer vbo = VertexBuffer::createObject(vertices, 32);
+	VertexBuffer vbo = VertexBuffer::createObject(vertices, 16);
 	VertexBufferLayout layout;
 
 	layout.add<float>(4);
-	layout.add<float>(4);
-	
-	VertexArray vao = VertexArray::createObject();
-	vao.addBuffer(vbo, layout);
+	//layout.add<float>(4);
 	
 	unsigned int indices[] = {
 		0, 1, 2,
@@ -55,18 +54,43 @@ int main(void)
 
 	IndexBuffer ibo = IndexBuffer::createObject(indices, 6);
 
+	float vertices2[] = {
+	0.25f, -0.25f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+	0.75f, -0.25f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+	0.25f, 0.25f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+	0.75f, 0.25f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f
+	};
+	VertexBuffer vbo2 = VertexBuffer::createObject(vertices2, 32);
+	VertexBufferLayout layout2;
+	layout2.add<float>(4);
+	layout2.add<float>(4);
+
+
+	VertexArray vao = VertexArray::createObject();
+	VertexArray vao2 = VertexArray::createObject();
+
+	vao.bind();
+	ibo.bind();
+	vao.addBuffer(vbo, layout);
+
+	vao2.bind();
+	ibo.bind();
+	vao2.addBuffer(vbo2, layout2);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		vbo.bind();
-		ibo.bind();
 		shader.bind();
-
-		vao.bind();
+		shader.setInt("u_set", 0);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		vao.bind();
+		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_INT, (void*)0);
+
+		vao2.bind();
+		shader.setInt("u_set", 1);
 		glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_INT, (void*)0);
 
 		/* Swap front and back buffers */
@@ -77,6 +101,9 @@ int main(void)
 	}
 
 	VertexBuffer::destroyObject(vbo);
+	VertexBuffer::destroyObject(vbo2);
+	VertexArray::destroyObject(vao);
+	VertexArray::destroyObject(vao2);
 	IndexBuffer::destroyObject(ibo);
 	Shader::destroyObject(shader);
 
