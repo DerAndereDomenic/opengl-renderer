28ad6d3a8e9b9d7a528782ae716a03dffddb67a7
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-09-29 14:58:36 +0200

    Add implementation of mesh

---
+++ OpenGL Renderer/src/DataStructure/impl/Mesh.cpp
@@ -0,0 +1,72 @@
+#include <DataStructure/Mesh.h>
+
+Mesh
+Mesh::createObject()
+{
+	Mesh result;
+
+	result._layout.add<float>(4);
+	result._layout.add<float>(4);
+	result._layout.add<float>(2);
+	result._vertex_count = 0;
+
+	return result;
+}
+
+void 
+Mesh::destroyObject(Mesh& mesh)
+{
+	VertexArray::destroyObject(mesh._vao);
+	VertexBuffer::destroyObject(mesh._vbo);
+	IndexBuffer::destroyObject(mesh._ibo);
+	mesh._vertex_count = 0;
+	mesh._indices.clear();
+	mesh._vertices.clear();
+}
+
+void 
+Mesh::create()
+{
+	_vbo = VertexBuffer::createObject(_vertices.data(), _vertices.size() * sizeof(float));
+	_ibo = IndexBuffer::createObject(_indices.data(), _indices.size());
+	_vbo.bind();
+	_ibo.bind();
+	_vao = VertexArray::createObject();
+	_vao.addBuffer(_vbo, _layout);
+
+	_vertices.clear();
+	_indices.clear();
+}
+
+void
+Mesh::render(RenderWindow window, Shader shader)
+{
+	window.render(_vao, _ibo, shader);
+}
+
+unsigned int 
+Mesh::addVertex(glm::vec4 position, glm::vec4 color, glm::vec2 texture)
+{
+	_vertices.push_back(position.x);
+	_vertices.push_back(position.y);
+	_vertices.push_back(position.z);
+	_vertices.push_back(position.w);
+	
+	_vertices.push_back(color.x);
+	_vertices.push_back(color.y);
+	_vertices.push_back(color.z);
+	_vertices.push_back(color.w);
+	
+	_vertices.push_back(texture.x);
+	_vertices.push_back(texture.y);
+
+	return _vertex_count++;
+}
+
+void 
+Mesh::addTriangle(const unsigned int vertex1, const unsigned int vertex2, const unsigned int vertex3)
+{
+	_indices.push_back(vertex1);
+	_indices.push_back(vertex2);
+	_indices.push_back(vertex3);
+}
