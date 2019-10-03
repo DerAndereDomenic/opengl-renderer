94dba5750086f39bd0e6d02f92fc120daca25720
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-10-03 18:23:50 +0200

    Add header for frame buffer

---
+++ OpenGL Renderer/src/OpenGLObjects/FrameBuffer.h
@@ -0,0 +1,47 @@
+#ifndef OPENGL_OBJECTS_FRAMEBUFFER_H
+#define OPENGL_OBJECTS_FRAMEBUFFER_H
+
+#include <OpenGLObjects/RenderBuffer.h>
+#include <OpenGLObjects/Texture.h>
+
+/**
+*	\brief A class to model a frame buffer object
+*/
+class Framebuffer
+{
+public:
+	/**
+	*	\brief Create an empty frame buffer object
+	*/
+	Framebuffer() = default;
+
+	/**
+	*	\brief Create a frame buffer object
+	*	\param[in] width The width of the buffer
+	*	\param[in] height The height of the buffer
+	*	\return The render buffer object
+	*/
+	static Framebuffer createObject(unsigned int width, unsigned int height);
+
+	/**
+	*	\brief Destroy the render buffer object
+	*	\param[in] object The object to be destroyed
+	*/
+	static void destroyObject(Framebuffer& object);
+
+	/**
+	*	\brief Binds the render buffer object
+	*/
+	void bind();
+
+	/**
+	*	\brief Unbinds the render buffer object (bind = 0)
+	*/
+	void unbind();
+private:
+	unsigned int _ID; /**<< The internal OpenGL handle for the fbo*/
+	Texture _render_texture; /**<< The Color texture of this fbo*/
+	RenderBuffer _depth_stencil; /**<< The depth and stencil buffer of this fbo*/
+};
+
+#endif
