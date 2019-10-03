27a7986083641d8ff22027bd91fe9f5fbbeaa2fb
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-10-03 22:13:59 +0200

    Add normals for every vertex creation

--- OpenGL Renderer/src/DataStructure/impl/MeshHelper.cpp
+++ OpenGL Renderer/src/DataStructure/impl/MeshHelper.cpp
@@ -21,50 +21,50 @@ MeshHelper::cubeMesh(glm::vec4 color)
 	glm::vec2 up(0, 1.0f);
 
 	//Front
-	v1 = mesh.addVertex(p1, color, tex);
-	v2 = mesh.addVertex(p2, color, tex + right);
-	v3 = mesh.addVertex(p3, color, tex + up + right);
-	v4 = mesh.addVertex(p4, color, tex + up);
+	v1 = mesh.addVertex(p1, color, tex, glm::vec3(0,0,1));
+	v2 = mesh.addVertex(p2, color, tex + right, glm::vec3(0, 0, 1));
+	v3 = mesh.addVertex(p3, color, tex + up + right, glm::vec3(0, 0, 1));
+	v4 = mesh.addVertex(p4, color, tex + up, glm::vec3(0, 0, 1));
 	mesh.addTriangle(v1, v2, v3);
 	mesh.addTriangle(v1, v3, v4);
 
 	//Back
-	v5 = mesh.addVertex(p5, color, tex);
-	v6 = mesh.addVertex(p6, color, tex + right);
-	v7 = mesh.addVertex(p7, color, tex + up + right);
-	v8 = mesh.addVertex(p8, color, tex + up);
+	v5 = mesh.addVertex(p5, color, tex, glm::vec3(0, 0, -1));
+	v6 = mesh.addVertex(p6, color, tex + right, glm::vec3(0, 0, -1));
+	v7 = mesh.addVertex(p7, color, tex + up + right, glm::vec3(0, 0, -1));
+	v8 = mesh.addVertex(p8, color, tex + up, glm::vec3(0, 0, -1));
 	mesh.addTriangle(v5, v6, v7);
 	mesh.addTriangle(v5, v7, v8);
 
 	//Right
-	v2 = mesh.addVertex(p2, color, tex);
-	v5 = mesh.addVertex(p5, color, tex + right);
-	v8 = mesh.addVertex(p8, color, tex + up + right);
-	v3 = mesh.addVertex(p3, color, tex + up);
+	v2 = mesh.addVertex(p2, color, tex, glm::vec3(1, 0, 0));
+	v5 = mesh.addVertex(p5, color, tex + right, glm::vec3(1, 0, 0));
+	v8 = mesh.addVertex(p8, color, tex + up + right, glm::vec3(1, 0, 0));
+	v3 = mesh.addVertex(p3, color, tex + up, glm::vec3(1, 0, 0));
 	mesh.addTriangle(v2, v5, v8);
 	mesh.addTriangle(v2, v8, v3);
 
 	//Left
-	v6 = mesh.addVertex(p6, color, tex);
-	v1 = mesh.addVertex(p1, color, tex + right);
-	v4 = mesh.addVertex(p4, color, tex + up + right);
-	v7 = mesh.addVertex(p7, color, tex + up);
+	v6 = mesh.addVertex(p6, color, tex, glm::vec3(-1, 0, 0));
+	v1 = mesh.addVertex(p1, color, tex + right, glm::vec3(-1, 0, 0));
+	v4 = mesh.addVertex(p4, color, tex + up + right, glm::vec3(-1, 0, 0));
+	v7 = mesh.addVertex(p7, color, tex + up, glm::vec3(-1, 0, 0));
 	mesh.addTriangle(v6, v1, v4);
 	mesh.addTriangle(v6, v4, v7);
 
 	//Top
-	v4 = mesh.addVertex(p4, color, tex);
-	v3 = mesh.addVertex(p3, color, tex + right);
-	v8 = mesh.addVertex(p8, color, tex + up + right);
-	v7 = mesh.addVertex(p7, color, tex + up);
+	v4 = mesh.addVertex(p4, color, tex, glm::vec3(0, 1, 0));
+	v3 = mesh.addVertex(p3, color, tex + right, glm::vec3(0, 1, 0));
+	v8 = mesh.addVertex(p8, color, tex + up + right, glm::vec3(0, 1, 0));
+	v7 = mesh.addVertex(p7, color, tex + up, glm::vec3(0, 1, 0));
 	mesh.addTriangle(v4, v3, v8);
 	mesh.addTriangle(v4, v8, v7);
 
 	//Bottom
-	v6 = mesh.addVertex(p6, color, tex);
-	v5 = mesh.addVertex(p5, color, tex + right);
-	v2 = mesh.addVertex(p2, color, tex + up + right);
-	v1 = mesh.addVertex(p1, color, tex + up);
+	v6 = mesh.addVertex(p6, color, tex, glm::vec3(0, -1, 0));
+	v5 = mesh.addVertex(p5, color, tex + right, glm::vec3(0, -1, 0));
+	v2 = mesh.addVertex(p2, color, tex + up + right, glm::vec3(0, -1, 0));
+	v1 = mesh.addVertex(p1, color, tex + up, glm::vec3(0, -1, 0));
 	mesh.addTriangle(v6, v5, v2);
 	mesh.addTriangle(v6, v2, v1);
 
