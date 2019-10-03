ddaf0b14d464b3e21d55918be9eed6cd3b9e65a3
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-10-03 20:46:57 +0200

    Add a utility Function to add a generic cube mesh

---
+++ OpenGL Renderer/src/DataStructure/MeshHelper.h
@@ -0,0 +1,19 @@
+#ifndef DATA_STRUCTURE_MESH_HELPER
+#define DATA_STRUCTURE_MESH_HELPER
+
+#include <DataStructure/Mesh.h>
+
+/**
+*	\brief A namespace to hold mesh utility functions
+*/
+namespace MeshHelper
+{
+	/**
+	*	\brief Creates a 1x1x1 cube at (0,0,0)
+	*	\return The cube mesh
+	*	\note: The mesh still has to be created
+	*/
+	Mesh cubeMesh();
+}
+
+#endif
