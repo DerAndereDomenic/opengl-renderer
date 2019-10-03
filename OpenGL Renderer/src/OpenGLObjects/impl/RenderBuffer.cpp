532583fb05354655ae65826eb024471bac1c18db
Domenic Zingsheim <domeniczingsheim@gmail.com>
2019-10-03 18:19:05 +0200

    Add Renderbuffer implementation

---
+++ OpenGL Renderer/src/OpenGLObjects/impl/RenderBuffer.cpp
@@ -0,0 +1,33 @@
+#include <OpenGLObjects/RenderBuffer.h>
+
+#include <GL/glew.h>
+
+RenderBuffer 
+RenderBuffer::createObject(unsigned int width, unsigned int height)
+{
+	RenderBuffer result;
+
+	glGenRenderbuffers(1, &result._ID);
+	glBindRenderbuffer(GL_RENDERBUFFER, result._ID);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+
+	return result;
+}
+
+void 
+RenderBuffer::destroyObject(RenderBuffer& rbo)
+{
+	glDeleteRenderbuffers(1, &rbo._ID);
+}
+
+void 
+RenderBuffer::bind()
+{
+	glBindRenderbuffer(GL_RENDERBUFFER, _ID);
+}
+
+void 
+RenderBuffer::unbind()
+{
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+}
