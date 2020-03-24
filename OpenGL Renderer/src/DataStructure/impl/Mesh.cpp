b46a9e25be357d9ea90379dfc95dcaa4c5e2b414
Domenic <domeniczingsheim@gmail.com>
2020-03-24 18:53:42 +0100

    Calculate and write tangent into vertex buffer

--- OpenGL Renderer/src/DataStructure/impl/Mesh.cpp
+++ OpenGL Renderer/src/DataStructure/impl/Mesh.cpp
@@ -102,4 +102,36 @@ Mesh::calculateTangent(const unsigned int index1, const unsigned int index2, uns
 		vertex2[i] = _vertices[id2 + i];
 		vertex3[i] = _vertices[id3 + i];
 	}
+
+	glm::vec3 pos1(vertex1[0], vertex1[1], vertex1[2]);
+	glm::vec3 pos2(vertex2[0], vertex2[1], vertex2[2]);
+	glm::vec3 pos3(vertex3[0], vertex3[1], vertex3[2]);
+
+	glm::vec2 uv1(vertex1[7], vertex1[8]);
+	glm::vec2 uv2(vertex2[7], vertex2[8]);
+	glm::vec2 uv3(vertex3[7], vertex3[8]);
+
+	//Calculating tangent
+	glm::vec3 edge1 = pos2 - pos1;
+	glm::vec3 edge2 = pos3 - pos1;
+
+	glm::vec2 deltaUV1 = uv2 - uv1;
+	glm::vec2 deltaUV2 = uv3 - uv1;
+
+	float det = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+	glm::vec3 tangent;
+	tangent.x = det * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+	tangent.y = det * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+	tangent.z = det * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+	tangent = glm::normalize(tangent);
+
+	//Write tangent into buffer
+	for (unsigned int i = 0; i < 3; ++i)
+	{
+		//Start of current vertex + position of tangent + current position in vector
+		_vertices[id1 + 12 + i] = tangent[i];
+		_vertices[id2 + 12 + i] = tangent[i];
+		_vertices[id3 + 12 + i] = tangent[i];
+	}
 }
