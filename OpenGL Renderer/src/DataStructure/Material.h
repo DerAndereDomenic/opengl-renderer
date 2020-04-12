145cbf66ad8f836a305b60a11db948b22d57ec36
Domenic <domeniczingsheim@gmail.com>
2020-04-12 23:17:26 +0200

    Add textures to material class

--- OpenGL Renderer/src/DataStructure/Material.h
+++ OpenGL Renderer/src/DataStructure/Material.h
@@ -2,15 +2,23 @@
 #define DATA_STRUCTURE_MATERIAL_H
 
 #include <glm/glm.hpp>
+#include "OpenGLObjects/Texture.h"
 
 /**
 *	\brief A struct to model a material
 */
 struct Material
 {
+	bool useTextures = false; /**<< Toggle the use of textures*/
 	glm::vec3 ambient; /**<< The ambient part of the material*/
 	glm::vec3 diffuse; /**<< The diffuse part of the material*/
 	glm::vec3 specular; /**<< The specular part of the material*/
+
+	Texture texture_diffuse; /**<< The diffuse texture*/
+	Texture texture_specular; /**<< The specular texture*/
+	Texture texture_normal; /**<< The normal map texture*/
+	Texture texture_height; /**<< The displacement map texture*/
+
 	float shininess; /**<< The shininess of the material*/
 };
 
