1ade31180aa927b6e88d5977df0657602bd1048f
Domenic <domeniczingsheim@gmail.com>
2020-04-13 15:16:34 +0200

    Implement bind method

--- OpenGL Renderer/src/DataStructure/impl/Material.cpp
+++ OpenGL Renderer/src/DataStructure/impl/Material.cpp
@@ -28,5 +28,25 @@ Material::destroyObject(Material& object)
 void 
 Material::bind(Shader shader)
 {
-	
+	shader.setBool(_name + ".useTextures", useTextures);
+	if (useTextures)
+	{
+		shader.setInt(_name + ".diffuse_map", 0);
+		shader.setInt(_name + ".specular_map", 1);
+		shader.setInt(_name + ".normal_map", 2);
+		shader.setInt(_name + ".height_map", 3);
+		shader.setInt(_name + ".shininess", shininess);
+
+		texture_diffuse.bind(0);
+		texture_specular.bind(1);
+		texture_normal.bind(2);
+		texture_height.bind(3);
+	}
+	else
+	{
+		shader.setVec3(_name + ".ambient", ambient);
+		shader.setVec3(_name + ".diffuse", diffuse);
+		shader.setVec3(_name + ".specular", specular);
+		shader.setFloat(_name + ".shininess", shininess);
+	}
 }
