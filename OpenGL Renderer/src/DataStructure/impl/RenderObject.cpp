463cf82dd144afdf217c587850291421bdd63dfe
Domenic <domeniczingsheim@gmail.com>
2020-04-15 14:34:59 +0200

    Implement createObject method

--- OpenGL Renderer/src/DataStructure/impl/RenderObject.cpp
+++ OpenGL Renderer/src/DataStructure/impl/RenderObject.cpp
@@ -4,7 +4,12 @@
 RenderObject 
 RenderObject::createObject(Mesh mesh, Material material, glm::mat4 model)
 {
+	RenderObject result;
+	result._mesh = mesh;
+	result._material = material;
+	result._model = model;
 
+	return result;
 }
 
 void 
