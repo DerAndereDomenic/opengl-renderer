2d48d2703a47588eafef52652f1f8f9a2c2c5d94
Domenic <domeniczingsheim@gmail.com>
2020-04-21 21:17:48 +0200

    Implement createObject method

---
+++ OpenGL Renderer/src/DataStructure/impl/EnvironmentMap.cpp
@@ -0,0 +1,25 @@
+#include <DataStructure/EnvironmentMap.h>
+
+EnvironmentMap 
+EnvironmentMap::createObject(glm::vec3 position)
+{
+	EnvironmentMap result;
+	result._camera = Camera::createObject(position, 90.0f, 1.0f, 0.1f, 100.0f);
+	result._cube_map = Texture::createObject(1000, 1000, CUBEMAP);
+	result._environment_map = FrameBuffer::createObject(1000, 1000);
+	result._environment_map.attachRenderBuffer();
+	result._environment_map.unbind();
+	return result;
+}
+
+void 
+EnvironmentMap::destroyObject(EnvironmentMap& object)
+{
+
+}
+
+void 
+EnvironmentMap::render(Scene scene, Shader shader)
+{
+
+}
