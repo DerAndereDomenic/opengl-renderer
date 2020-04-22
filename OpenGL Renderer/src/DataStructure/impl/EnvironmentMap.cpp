40548f3e62db14254a04cb4d5c70dc7801c28544
Domenic <domeniczingsheim@gmail.com>
2020-04-22 17:30:20 +0200

    Implement method to render environment map

--- OpenGL Renderer/src/DataStructure/impl/EnvironmentMap.cpp
+++ OpenGL Renderer/src/DataStructure/impl/EnvironmentMap.cpp
@@ -5,8 +5,8 @@ EnvironmentMap::createObject(glm::vec3 position)
 {
 	EnvironmentMap result;
 	result._camera = Camera::createObject(position, 90.0f, 1.0f, 0.1f, 100.0f);
-	result._cube_map = Texture::createObject(1000, 1000, CUBEMAP);
-	result._environment_map = FrameBuffer::createObject(1000, 1000);
+	result._cube_map = Texture::createObject(1024, 1024, CUBEMAP);
+	result._environment_map = FrameBuffer::createObject(1024, 1024);
 	result._environment_map.attachRenderBuffer();
 	result._environment_map.unbind();
 	return result;
@@ -23,5 +23,16 @@ EnvironmentMap::destroyObject(EnvironmentMap& object)
 void 
 EnvironmentMap::render(Scene scene, Shader shader)
 {
-
+	_environment_map.bind();
+	glViewport(0, 0, 1024, 1024);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	for (unsigned int i = 0; i < 6; ++i)
+	{
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, _cube_map.getID(), 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		_camera.updateDirection(angles[i].pitch, angles[i].yaw);
+		shader.setMVP(glm::mat4(1), _camera.getView(), _camera.getProjection());
+		scene.render(shader);
+	}
+	_environment_map.unbind();
 }
