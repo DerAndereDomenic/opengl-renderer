c9ef71d2b828ac3c7bc653abd9d02567acb2c399
Domenic <domeniczingsheim@gmail.com>
2020-04-22 17:47:06 +0200

    Implement destroy and render method

--- OpenGL Renderer/src/DataStructure/impl/Skybox.cpp
+++ OpenGL Renderer/src/DataStructure/impl/Skybox.cpp
@@ -16,11 +16,20 @@ Skybox::createObject(Texture cubemap)
 void 
 Skybox::destroyObject(Skybox& skybox)
 {
-
+	Texture::destroyObject(skybox._cubemap);
+	Mesh::destroyObject(skybox._cube);
+	Shader::destroyObject(skybox._shader);
 }
 
 void
 Skybox::render(Camera camera) 
 {
-
+	glDepthMask(GL_FALSE);
+	_shader.bind();
+	_cubemap.bind(0);
+	_shader.setMVP(glm::mat4(1),
+		glm::mat4(glm::mat3(camera.getView())),
+		camera.getProjection());
+	_cube.render();
+	glDepthMask(GL_TRUE);
 }
