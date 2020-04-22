45ea4670a06a53e4df7927b2280c53bc68d1fd68
Domenic <domeniczingsheim@gmail.com>
2020-04-22 17:42:02 +0200

    Add header for a skybox class

---
+++ OpenGL Renderer/src/DataStructure/Skybox.h
@@ -0,0 +1,40 @@
+#ifndef DATASTRUCTURE_SKYBOX_H
+#define DATASTRUCTURE_SKYBOX_H
+
+#include <DataStructure/Mesh.h>
+#include <OpenGLObjects/Texture.h>
+#include <Shader/Shader.h>
+#include <Renderer/Camera.h>
+
+class Skybox
+{
+public:
+	/**
+	*	\brief Creates an empty skybox
+	*/
+	Skybox() = default;
+
+	/**
+	*	\brief Create a skybox with the given texture
+	*	\return A skybox
+	*/
+	static Skybox createObject(Texture cubemap);
+
+	/**
+	*	\brief Destroys the skybox and it's texture
+	*/
+	static void destroyObject(Skybox& skybox);
+
+	/**
+	*	\brief Renders the skybox to the given camera
+	*	\param[in] camera The camera class
+	*/
+	void render(Camera camera);
+
+private:
+	Mesh _cube; /**<< A generic cube Mesh to render the skybox*/
+	Texture _cubemap; /**<< The texture of the skybox*/
+	Shader _shader; /**<< The skybox shader*/
+};
+
+#endif
