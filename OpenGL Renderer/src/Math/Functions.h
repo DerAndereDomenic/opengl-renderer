2e76cdae0f3bddf2ff8bf21dcf053264e62c58a5
Domenic <domeniczingsheim@gmail.com>
2020-04-26 14:52:35 +0200

    Add comments

--- OpenGL Renderer/src/Math/Functions.h
+++ OpenGL Renderer/src/Math/Functions.h
@@ -3,6 +3,14 @@
 
 namespace Math
 {
+	/**
+	*	\brief Interpolates between a and b
+	*	\tparam T The type
+	*	\param[in] a The first value
+	*	\param[in] b The second value
+	*	\param[in] t The interpolation step
+	*	\return a(1-t)+tb
+	*/
 	template<typename T>
 	T lerp(T a, T b, float t);
 };
