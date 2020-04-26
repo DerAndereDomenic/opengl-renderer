bf15c01b9954a3969b2e548b6b0c3f0685dd2737
Domenic <domeniczingsheim@gmail.com>
2020-04-26 14:53:29 +0200

    Add lerp implementation

--- OpenGL Renderer/src/Math/impl/FunctionsDetail.h
+++ OpenGL Renderer/src/Math/impl/FunctionsDetail.h
@@ -1,6 +1,11 @@
 #ifndef MATH_IMPL_FUNCTIONSDETAIL_H
 #define MATH_IMPL_FUNCTIONSDETAIL_H
 
-
+template<typename T>
+T
+Math::lerp(T a, T b, float t)
+{
+	return a * (1.0f - t) + b * t;
+}
 
 #endif
