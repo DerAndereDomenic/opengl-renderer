e7066029aa72b160d6ca9811aa00b81b72d2a7e1
Domenic <domeniczingsheim@gmail.com>
2020-06-19 18:08:36 +0200

    Implement dummy functions

--- OpenGL Renderer/src/Shader/impl/ShaderManager.cpp
+++ OpenGL Renderer/src/Shader/impl/ShaderManager.cpp
@@ -1,1 +1,27 @@
 #include <Shader/ShaderManager.h>
+
+
+ShaderManager
+ShaderManager::createObject()
+{
+
+}
+
+void
+ShaderManager::destroyObject(ShaderManager& manager)
+{
+
+}
+
+
+void
+ShaderManager::addShader(const std::string& name)
+{
+
+}
+
+Shader
+ShaderManager::getShader(const std::string& name)
+{
+
+}
