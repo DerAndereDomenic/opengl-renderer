daa55df438ec54a8ee8a973e58161f68e2b6057e
Domenic <domeniczingsheim@gmail.com>
2020-07-23 17:54:21 +0200

    Add TUMPose

---
+++ OpenGL Renderer/src/DataStructure/TUMPose.h
@@ -0,0 +1,12 @@
+#ifndef DATA_STRUCTURE_TUMPOSE_H
+#define DATA_STRUCTURE_TUMPOSE_H
+
+#include <glm/glm.hpp>
+
+struct TUMPose
+{
+	glm::vec3 _translation; /**< The translation vector of the camera pose*/
+	glm::fquat _rotation;	/**< The rotation of the camera pose as quaternion*/
+};
+
+#endif
