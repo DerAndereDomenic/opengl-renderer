67ec054a757571672f86645b8fe0f74bb87d862c
Domenic <domeniczingsheim@gmail.com>
2020-07-23 18:19:00 +0200

    Cherry pick git ignore and framebuffer extension from dataset_rendering

--- OpenGL Renderer/src/OpenGLObjects/impl/FrameBuffer.cpp
+++ OpenGL Renderer/src/OpenGLObjects/impl/FrameBuffer.cpp
@@ -40,6 +40,14 @@ FrameBuffer::attachColor()
 }
 
 void
+FrameBuffer::attachColor(Texture color_buffer)
+{
+	bind();
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + _render_textures.size(), GL_TEXTURE_2D, color_buffer.getID(), 0);
+	_render_textures.push_back(color_buffer);
+}
+
+void
 FrameBuffer::attachRenderBuffer()
 {
 	bind();
