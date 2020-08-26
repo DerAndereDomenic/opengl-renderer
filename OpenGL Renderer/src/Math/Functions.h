d5301553e10669eb3ab0dfce4abede070ecbc374
Domenic <domeniczingsheim@gmail.com>
2020-08-27 00:34:53 +0200

    Add documentation

--- OpenGL Renderer/src/Math/Functions.h
+++ OpenGL Renderer/src/Math/Functions.h
@@ -14,9 +14,23 @@ namespace Math
 	template<typename T>
 	T lerp(T a, T b, float t);
 
+	/**
+	*	\brief A smoothstep function used for interpolation
+	*	\param[in] x The interpolation parameter
+	*	\return The smoothed interpolation parameter
+	*	\note: See https://en.wikipedia.org/wiki/Smoothstep
+	*/
 	template<typename T>
 	T smoothstep(T x);
 
+	/**
+	*	\brief A smooth linear interpolation
+	*	\param[in] a The first value
+	*	\param[in] b The second value
+	*	\param[in] t The interpolation step
+	*	\return The interpolated value
+	*	\note: See https://en.wikipedia.org/wiki/Perlin_noise
+	*/
 	template<typename T>
 	T smoothlerp(T a, T b, T t);
 };
