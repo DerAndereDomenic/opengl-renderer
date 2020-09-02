dae223cf6fff904916e7fb4725b04ade72612a5b
Domenic <domeniczingsheim@gmail.com>
2020-09-02 22:27:54 +0200

    Add reading font from disk

--- OpenGL Renderer/src/Renderer/impl/TextRenderer.cpp
+++ OpenGL Renderer/src/Renderer/impl/TextRenderer.cpp
@@ -23,5 +23,12 @@ TextRenderer::destroyObject(TextRenderer& object)
 void 
 TextRenderer::loadFont(const char* path, const unsigned int number_char, const unsigned int font_size)
 {
+	if (FT_New_Face(_ft, path, 0, &_face))
+	{
+		std::cout << "ERROR::TEXTRENDERER::FREETYPE: Could not load font at: " << path << std::endl;
+	}
 
+	FT_Set_Pixel_Sizes(_face, 0, font_size);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); //disable byte-alignment restriction;
 }
