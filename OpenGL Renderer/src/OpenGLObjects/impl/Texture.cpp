26d74baf70afee6c84252d61dc76d84ab2d48d0b
Domenic <domeniczingsheim@gmail.com>
2020-09-02 22:00:06 +0200

    Move createTexture based on parameters into the detail header

--- OpenGL Renderer/src/OpenGLObjects/impl/Texture.cpp
+++ OpenGL Renderer/src/OpenGLObjects/impl/Texture.cpp
@@ -117,40 +117,6 @@ Texture::createObject(float* data, unsigned int width, unsigned int height)
 	return result;
 }
 
-Texture 
-Texture::createObject(const unsigned int width, 
-					  const unsigned int height, 
-					  const unsigned int target, 
-					  const unsigned int internal_format, 
-					  const unsigned int format,
-					  const unsigned int type)
-{
-	Texture result;
-	glGenTextures(1, &result._ID);
-	glBindTexture(target, result._ID);
-	if (target == TEXTURE)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, internal_format, width, height, 0, format, type, NULL);
-
-	}
-	else
-	{
-		for (unsigned int i = 0; i < 6; ++i) 
-		{
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internal_format, width, height, 0, format, type, NULL);
-		}
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	}
-	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	result._target = target;
-
-	return result;
-}
-
 void 
 Texture::destroyObject(Texture& texture)
 {
