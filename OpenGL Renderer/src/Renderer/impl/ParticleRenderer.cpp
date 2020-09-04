1d0d6dd1a8d0f54f660db66e76f1a06c5f19be09
Domenic <domeniczingsheim@gmail.com>
2020-09-05 01:53:16 +0200

    Sample upper hemisphere as y = Up axis

--- OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
+++ OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
@@ -8,11 +8,11 @@ Particle::Particle(glm::vec3 position)
 	float angle = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX)*3.14159f*2.0f;
 	float r = sqrtf(static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX));
 	float x = r * cos(angle);
-	float y = r * sin(angle);
+	float z = r * sin(angle);
 
-	float z = sqrtf(fmax(0.0f, 1 - x * x - y * y));
+	float y = sqrtf(fmax(0.0f, 1 - x * x - z * z));
 
-	velocity = glm::vec3(0, 1, 0);// static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * glm::vec3(x, y, z);
+	velocity = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * glm::vec3(x, y, z);
 }
 
 ParticleRenderer 
