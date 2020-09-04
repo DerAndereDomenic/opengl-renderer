272cf39c16a6299984e197bfe46dd9ff5f64a9f1
Domenic <domeniczingsheim@gmail.com>
2020-09-05 00:51:48 +0200

    Calculate particle velocity by sampling the hemisphere cosine weighted

--- OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
+++ OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
@@ -1,9 +1,17 @@
 #include <Renderer/ParticleRenderer.h>
+#include <ctime>
 
 Particle::Particle(glm::vec3 position)
 	:position(position)
 {
+	float angle = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX)*3.14159f*2.0f;
+	float r = sqrtf(static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX));
+	float x = r * cos(angle);
+	float y = r * sin(angle);
 
+	float z = sqrtf(fmax(0.0f, 1 - x * x - y * y));
+
+	velocity = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * glm::vec3(x, y, z);
 }
 
 ParticleRenderer 
@@ -12,6 +20,8 @@ ParticleRenderer::createObject(const unsigned int num_particles, Texture texture
 	ParticleRenderer result;
 
 	result._texture = texture;
+
+	std::srand(std::time(nullptr));
 
 	return result;
 }
