2631f478acedb893f7fcd7efc46572a00920ef80
Domenic <domeniczingsheim@gmail.com>
2020-09-05 00:40:28 +0200

    Setup a basic ParticleRenderer class

---
+++ OpenGL Renderer/src/Renderer/ParticleRenderer.h
@@ -0,0 +1,33 @@
+#ifndef RENDERER_PARTICLERENDERER_H
+#define RENDERER_PARTICLERENDERER_H
+
+#include <vector>
+#include <OpenGLObjects/Texture.h>
+#include <glm/glm.hpp>
+
+struct Particle
+{
+	glm::vec3 position;
+	glm::vec3 velocity;
+
+	float timeAlive;
+};
+
+class ParticleRenderer
+{
+	public:
+		ParticleRenderer() = default;
+
+		static ParticleRenderer createObject();
+
+		static void destroyObject(ParticleRenderer& object);
+
+		void update();
+
+		void render();
+	private:
+		Texture _texture;
+		std::vector<Particle> _particles;
+};
+
+#endif
