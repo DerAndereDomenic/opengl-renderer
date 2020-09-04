33abc3be10d3a7a164e27bf83c902c3ca2ea2c88
Domenic <domeniczingsheim@gmail.com>
2020-09-05 00:42:25 +0200

    Add basic implementation of the ParticleRenderer

---
+++ OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
@@ -0,0 +1,27 @@
+#include <Renderer/ParticleRenderer.h>
+
+ParticleRenderer 
+ParticleRenderer::createObject()
+{
+	ParticleRenderer result;
+
+	return result;
+}
+
+void 
+destroyObject(ParticleRenderer& object)
+{
+
+}
+
+void 
+ParticleRenderer::update()
+{
+
+}
+
+void 
+ParticleRenderer::render()
+{
+
+}
