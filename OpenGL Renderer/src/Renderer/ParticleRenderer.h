2c036e041690f1a5f0172b3363ac155f8be3858b
Domenic <domeniczingsheim@gmail.com>
2020-09-05 16:11:02 +0200

    Add documentation

--- OpenGL Renderer/src/Renderer/ParticleRenderer.h
+++ OpenGL Renderer/src/Renderer/ParticleRenderer.h
@@ -9,34 +9,63 @@
 
 struct Particle
 {
-	glm::vec3 position;
-	glm::vec3 velocity;
+	glm::vec3 position;									/**<< The position of the particle in world space*/
+	glm::vec3 velocity;									/**<< The velocity of the particle in world space*/
 
-	float timeAlive = 0.0f;
+	float timeAlive = 0.0f;								/**<< The time this particle has to live*/
 
+	/**
+	*	\brief Creates a new particle
+	*	\param[in] position The position of the particle in world space
+	*	\param[in] time_alive The maximum time the particle should stay alive
+	*/
 	Particle(glm::vec3 position, float time_alive);
 };
 
 class ParticleRenderer
 {
 	public:
+		/**
+		*	\brief Default constructor
+		*/
 		ParticleRenderer() = default;
 
+		/**
+		*	\brief Creates a ParticleRenderer
+		*	\param[in] position The position of the emitter in world space
+		*	\param[in] num_particles The number of particles alive at a time
+		*	\param[in] time_alive The maximum time a particle should stay alive
+		*	\param[in] texture The texture of an particle
+		*	\return The Particle emitter
+		*/
 		static ParticleRenderer createObject(glm::vec3 position, const unsigned int num_particles, float time_alive, Texture texture);
 
+		/**
+		*	\brief Destroys the object
+		*	\param[in] object The object to be destroyed
+		*/
 		static void destroyObject(ParticleRenderer& object);
 
+		/**
+		*	\brief Updates the particle system
+		*	\param[in] deltaTime The time between two frames
+		*/
 		void update(float deltaTime);
 
+		/**
+		*	\brief Renders the particle system
+		*	\param[in] camera The camera that is observing the particle system
+		*	\note: This function disables depth writing for the draw call. The system should be rendered last in the scene
+		*/
 		void render(Camera& camera);
 	private:
-		Texture _texture;
-		std::vector<Particle> _particles;
-		VertexArray _vao;
-		VertexBuffer _instanceArray;
-		glm::vec3 _position;
-		float _timeAlive = 0.0f;
-		float* _positions = nullptr;
+		Texture _texture;					/**<< The texture of a single particle*/
+		std::vector<Particle> _particles;	/**<< The particles managed by this system*/
+		VertexArray _vao;					/**<< The internal vao of the particle system*/
+		VertexBuffer _instanceArray;		/**<< The data needed for each particle*/
+		glm::vec3 _position;				/**<< The position of the emitter in world space*/
+		float _timeAlive = 0.0f;			/**<< The maximum time a particle can stay alive*/
+		float* _positions = nullptr;		/**<< Internal buffer to update the vbo*/
 };
 
 #endif
