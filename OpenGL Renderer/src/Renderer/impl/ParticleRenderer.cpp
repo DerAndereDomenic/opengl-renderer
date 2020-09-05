ac03e38a52d9f57e8fc09c768a05b8aaf47a8d02
Domenic <domeniczingsheim@gmail.com>
2020-09-05 17:00:04 +0200

    Remove color of particle

--- OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
+++ OpenGL Renderer/src/Renderer/impl/ParticleRenderer.cpp
@@ -14,7 +14,6 @@ Particle::Particle(glm::vec3 position, float time_alive)
 
 	velocity = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX) * glm::vec3(x, y, z);
 	timeAlive = static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
-	color = glm::vec4(1, 1, 1, 1);
 }
 
 ParticleRenderer 
@@ -66,7 +65,6 @@ ParticleRenderer::update(float deltaTime)
 	{
 		_particles[i].timeAlive -= deltaTime;
 		_particles[i].position += deltaTime * _particles[i].velocity;
-		_particles[i].color -= deltaTime;
 		if (_particles[i].timeAlive <= 0)
 		{
 			_particles[i] = Particle(_position, _timeAlive);
