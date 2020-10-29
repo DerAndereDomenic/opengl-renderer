7bfd88204336f32c80f40a9b8831a82fccff36e9
Domenic <domeniczingsheim@gmail.com>
2020-10-29 23:17:10 +0100

    Initialize VR

--- lib/OpenGL Renderer/src/Renderer/impl/VRRenderer.cpp
+++ lib/OpenGL Renderer/src/Renderer/impl/VRRenderer.cpp
@@ -1,9 +1,22 @@
 #include <Renderer/VRRenderer.h>
+#include <DLogger/Logger.h>
+#include <string>
 
 VRRenderer 
 VRRenderer::createObject()
 {
+    VRRenderer result;
 
+    vr::EVRInitError error = vr::VRInitError_None;
+    result._vr_pointer = vr::VR_Init(&error, vr::VRApplication_Scene);
+    if(error != vr::VRInitError_None)
+    {
+        result._vr_pointer = NULL;
+        LOGGER::ERROR("Unable to init VR runtime: " + std::string(vr::VR_GetVRInitErrorAsEnglishDescription(error)) + "\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return result;
 }
 
 void 
