91fd73f62c97baf54fe82306e86edc48f39df3c6
Domenic <domeniczingsheim@gmail.com>
2020-10-29 23:04:28 +0100

    Add source file

---
+++ lib/OpenGL Renderer/src/Renderer/impl/VRRenderer.cpp
@@ -0,0 +1,19 @@
+#include <Renderer/VRRenderer.h>
+
+VRRenderer 
+VRRenderer::createObject()
+{
+
+}
+
+void 
+VRRenderer::destroyObject(VRRenderer& object)
+{
+
+}
+
+void 
+VRRenderer::spinOnce()
+{
+    
+}
