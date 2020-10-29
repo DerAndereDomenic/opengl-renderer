405251af26b2c37ad399634d299590dac95c0949
Domenic <domeniczingsheim@gmail.com>
2020-10-29 23:01:05 +0100

    Add basic member variables and functions

--- lib/OpenGL Renderer/src/Renderer/VRRenderer.h
+++ lib/OpenGL Renderer/src/Renderer/VRRenderer.h
@@ -3,4 +3,18 @@
 
 #include <openvr.h>
 
+class VRRenderer
+{
+    public:
+        VRRenderer() = default;
+
+        static VRRenderer createObject();
+
+        static void destroyObject(VRRenderer& object);
+
+        void spinOnce();
+    private:
+        vr::IVRSystem* vr_pointer = NULL;
+};
+
 #endif
