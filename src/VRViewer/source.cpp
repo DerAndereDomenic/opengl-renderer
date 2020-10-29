3414b21edb0fc0b0d8610e9a79fb71b4cd29ee5f
Domenic <domeniczingsheim@gmail.com>
2020-10-29 23:30:20 +0100

    Create a renderer object

--- src/VRViewer/source.cpp
+++ src/VRViewer/source.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <Renderer/VRRenderer.h>
+#include <DLogger/Logger.h>
 
 int main()
 {
+	LOGGER::setProject("VR Viewer", "1.0");
+	LOGGER::start();
+	VRRenderer renderer = VRRenderer::createObject();
+
+	LOGGER::end();
 	return 0;
 }
