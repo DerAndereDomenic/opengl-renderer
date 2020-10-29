cbab7d333bc5effab2254e94661e19b535800b01
Domenic <domeniczingsheim@gmail.com>
2020-10-29 23:39:38 +0100

    Docs: Add documentation for vr renderer

--- lib/OpenGL Renderer/src/Renderer/VRRenderer.h
+++ lib/OpenGL Renderer/src/Renderer/VRRenderer.h
@@ -6,17 +6,35 @@
 class VRRenderer
 {
     public:
+        /**
+         *  \brief Default constructor 
+         */
         VRRenderer() = default;
 
+        /**
+         *  \brief Creates the VR invironment and returns the renderer
+         *  \return The VR renderer
+         */
         static VRRenderer createObject();
 
+        /**
+         *  \brief Destroys the renderer and closes the VR context
+         *  \param[in] object The object to be destroyed
+         */ 
         static void destroyObject(VRRenderer& object);
 
+        /**
+         *  \brief Handles vr events 
+         */
         void spinOnce();
     private:
+        /**
+         *  \brief Handles the event given by the vr context
+         *  \param[in] event The event to handle
+         */
         void handleEvents(const vr::VREvent_t& event);
 
-        vr::IVRSystem* _vr_pointer = NULL;
+        vr::IVRSystem* _vr_pointer = NULL;      /**<< The VR context */
 };
 
 #endif
