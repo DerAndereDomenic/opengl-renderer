350405b7500ba87351d8b4cab2c3515d78898767
Domenic <domeniczingsheim@gmail.com>
2020-10-31 18:17:33 +0100

    Add dummy implementations

--- lib/OpenGL Renderer/src/Core/impl/GLFunctions.cpp
+++ lib/OpenGL Renderer/src/Core/impl/GLFunctions.cpp
@@ -1,1 +1,26 @@
 #include <Core/GLFunctions.h>
+
+
+void 
+GL::clear(const GLclampf& r = 0, const GLclampf& g = 0, const GLclampf& b = 0, const GLclampf& a = 1)
+{
+    
+}
+
+void 
+GL::setViewport(const uint32_t& width, const uint32_t& height)
+{
+
+}
+
+void 
+GL::enableDepthWriting()
+{
+
+}
+
+void 
+GL::disableDepthWriting()
+{
+    
+}
