ff4dbdd82abde5f9b324d1093ed9762c448b7ec4
Domenic <domeniczingsheim@gmail.com>
2020-11-01 19:23:43 +0100

    Render colors to quad

--- src/VRViewer/source.cpp
+++ src/VRViewer/source.cpp
@@ -4,6 +4,7 @@
 #include <IO/KeyManager.h>
 #include <Core/GLFunctions.h>
 #include <Shader/ShaderManager.h>
+#include <DataStructure/MeshHelper.h>
 
 int main()
 {
@@ -17,16 +18,25 @@ int main()
 
 	ShaderManager::instance()->addShader("BasicVR");
 
+	Mesh quad = MeshHelper::quadMesh(2.0f);
+	quad.create();
+
 	bool running = true;
 
 	while (running && dummy_window.isOpen())
 	{
+		renderer.getRenderTarget().bind();
+		GL::clear();
+		ShaderManager::instance()->getShader("BasicVR").bind();
+		quad.render();
 
 		renderer.render();
+		
+		FrameBuffer::bindDefault();
 
 		renderer.spinOnce();
 		dummy_window.spinOnce();
-		
+
 		if (KeyManager::instance()->isKeyDown(GLFW_KEY_ESCAPE))
 		{
 			running = false;
@@ -37,6 +47,8 @@ int main()
 	RenderWindow::destroyObject(dummy_window);
 	VRRenderer::destroyObject(renderer);
 	KeyManager::instance()->destroy();
+	Mesh::destroyObject(quad);
+	ShaderManager::destroyObject(*ShaderManager::instance());
 
 	LOGGER::end();
 	return 0;
