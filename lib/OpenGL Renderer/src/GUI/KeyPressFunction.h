ac52048b6c3421519fe50d5af8fa68dfcf6c99aa
Domenic <domeniczingsheim@gmail.com>
2020-11-17 20:01:41 +0100

    Add parameter to get access to the invoked key

--- lib/OpenGL Renderer/src/GUI/KeyPressFunction.h
+++ lib/OpenGL Renderer/src/GUI/KeyPressFunction.h
@@ -1,13 +1,16 @@
 #ifndef __OPENGLRENDERER_GUI_KEYPRESSFUNCTION_H
 #define __OPENGLRENDERER_GUI_KEYPRESSFUNCTION_H
 
+#include <cstdint>
+
 class KeyPressFunction
 {
 	public:
 		/**
 		*	\brief The function called when a key press is invoked.
+		*	\param[in] key The invoked key (default = 0)
 		*/
-		virtual void onPress() = 0;
+		virtual void onPress(const uint32_t& key = 0) = 0;
 };
 
 #endif
