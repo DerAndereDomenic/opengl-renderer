5fd24bdf5b0778e57dffc4fd8f9552de16ed31fd
Domenic <domeniczingsheim@gmail.com>
2020-11-18 00:43:20 +0100

    Add Callback for the mode control

---
+++ lib/OpenGL Renderer/src/GUI/ModeControl.h
@@ -0,0 +1,23 @@
+#ifndef __OPENGLRENDERER_GUI_MODECONTROL_H
+#define __OPENGLRENDERER_GUI_MODECONTROL_H
+
+class RenderWindow;
+#include <GUI/KeyPressFunction.h>
+
+class ModeControl : public KeyPressFunction
+{
+	public:
+		/**
+		*	\brief Construct a Mode control callback
+		*	\param[in] window The window
+		*/
+		ModeControl(RenderWindow* window);
+
+		//Override
+		void onPress(const uint32_t& key);
+
+	private:
+		RenderWindow* window = nullptr;		/**<< The window */
+};
+
+#endif
