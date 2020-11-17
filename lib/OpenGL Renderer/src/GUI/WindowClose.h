0bd32a64d6262c25f59e4bda23978dd532fd9fc0
Domenic <domeniczingsheim@gmail.com>
2020-11-17 20:08:24 +0100

    Add header for Window close callback

---
+++ lib/OpenGL Renderer/src/GUI/WindowClose.h
@@ -0,0 +1,22 @@
+#ifndef __OPENGLRENDERER_GUI_WINDOWCLOSE_H
+#define __OPENGLRENDERER_GUI_WINDOWCLOSE_H
+
+#include <Renderer/RenderWindow.h>
+#include <GUI/KeyPressFunction.h>
+
+class WindowClose : public KeyPressFunction
+{
+	public:
+		/**
+		*	\brief Creates a Window close callback
+		*	\param[in] window The window to close
+		*/
+		WindowClose(RenderWindow* window);
+
+	//Override
+	virtual void onPress(const int32_t& key);
+	private:
+		RenderWindow* _window = nullptr;	/**<< The window to close*/
+};
+
+#endif
