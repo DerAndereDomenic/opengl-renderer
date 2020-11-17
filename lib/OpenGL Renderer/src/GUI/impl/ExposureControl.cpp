570f12d0d89c2b2d5e77067a081037ed651ef6e0
Domenic <domeniczingsheim@gmail.com>
2020-11-17 20:17:29 +0100

    Implement Exposure control callback

---
+++ lib/OpenGL Renderer/src/GUI/impl/ExposureControl.cpp
@@ -0,0 +1,20 @@
+#include <GUI/ExposureControl.h>
+#include <GLFW/glfw3.h>
+
+void 
+ExposureControl::onPress(const uint32_t& key)
+{
+	if (key == GLFW_KEY_KP_ADD)
+	{
+		_exposure += 0.01f;
+	}
+	
+	if (GLFW_KEY_KP_SUBTRACT)
+	{
+		if (_exposure > 0.01f)
+		{
+			_exposure -= 0.01f;
+		}
+	}
+	
+}
