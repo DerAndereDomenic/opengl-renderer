c1cb7386ffd81f6648785113c474b62244962c09
Domenic <domeniczingsheim@gmail.com>
2020-11-17 20:21:44 +0100

    Add header for debug callback

---
+++ lib/OpenGL Renderer/src/GUI/DebugControl.h
@@ -0,0 +1,26 @@
+#ifndef __OPENGLRENDERER_GUI_DEBUGCONTROL_H
+#define __OPENGLRENDERER_GUI_DEBUGCONTROL_H
+
+#include <GUI/KeyPressFunction.h>
+
+class DebugControl : public KeyPressFunction
+{
+	public:
+		/**
+		*	\brief Default constructor
+		*/
+		DebugControl() = default;
+
+		//Override
+		virtual void onPress(const uint32_t& key);
+
+		/**
+		*	\brief Get the debug mode
+		*	\return Wether debug is enabled or not
+		*/
+		inline bool getDebug() { return _debug; }
+	private:
+		bool _debug = false;	/**<< The debug level*/
+};
+
+#endif
