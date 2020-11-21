8dc20b93caf4b6e1d204371365d82190650b3436
Domenic <domeniczingsheim@gmail.com>
2020-11-21 11:36:59 +0100

    Add default constructor

--- lib/OpenGL Renderer/src/GUI/Button.h
+++ lib/OpenGL Renderer/src/GUI/Button.h
@@ -3,23 +3,26 @@
 
 #include <cstdint>
 #include <Renderer/TextRenderer.h>
+#include <string>
 
 class Button
 {
 	public:
+		Button() = default;
+
 		Button(const uint32_t& x, const uint32_t& y, const uint32_t& width, const uint32_t& height);
 
 		void setText(const std::string& text);
 
 		void render(const TextRenderer& renderer);
 	private:
-		uint32_t _poxX;
-		uint32_t _posY;
+		uint32_t _posX = 0;
+		uint32_t _posY = 0;
 
-		uint32_t _width;
-		uint32_t _height;
+		uint32_t _width = 0;
+		uint32_t _height = 0;
 
-		std::string _text;
+		std::string _text = std::string();
 };
 
 #endif
