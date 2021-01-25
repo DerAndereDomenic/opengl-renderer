20e767657955eb273316121616d92f3ef185aa21
Domenic <domeniczingsheim@gmail.com>
2021-01-25 23:47:47 +0100

    Add dlogger as submodule to be cloned when executing the build script

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 BUILD_DIR="build"
+DLOGGER_DIR="lib/DLogger"
 
 if [ ! -d "$BUILD_DIR" ]; then
 	echo "Creating build directory!";
 	mkdir "$BUILD_DIR";
+fi
+
+if [ ! -d "$DLOGGER_DIR" ]; then
+	echo "Cloning DLogger...";
+	cd lib
+	git clone --depth 1 https://gitlab.com/DerAndereDomenic/dlogger.git 
+	cd ..
 fi
 
 cd "$BUILD_DIR"
