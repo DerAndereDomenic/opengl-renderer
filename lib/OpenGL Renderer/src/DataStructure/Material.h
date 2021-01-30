099d254ded425ebc640f829df26507afc6757109
Domenic <domeniczingsheim@gmail.com>
2021-01-31 00:06:01 +0100

    Add roughness and refractive index to material

--- lib/OpenGL Renderer/src/DataStructure/Material.h
+++ lib/OpenGL Renderer/src/DataStructure/Material.h
@@ -42,19 +42,21 @@ public:
 	*/
 	void bind(Shader shader);
 
-	bool useTextures = false; /**<< Toggle the use of textures*/
-	glm::vec3 ambient = glm::vec3(0, 0, 0); /**<< The ambient part of the material*/
-	glm::vec3 diffuse = glm::vec3(0, 0, 0); /**<< The diffuse part of the material*/
-	glm::vec3 specular = glm::vec3(0, 0, 0); /**<< The specular part of the material*/
+	bool useTextures = false;					/**<< Toggle the use of textures*/
+	glm::vec3 ambient = glm::vec3(0, 0, 0);		/**<< The ambient part of the material*/
+	glm::vec3 diffuse = glm::vec3(0, 0, 0);		/**<< The diffuse part of the material*/
+	glm::vec3 specular = glm::vec3(0, 0, 0);	/**<< The specular part of the material*/
 
-	Texture texture_diffuse; /**<< The diffuse texture*/
-	Texture texture_specular; /**<< The specular texture*/
-	Texture texture_normal; /**<< The normal map texture*/
-	Texture texture_height; /**<< The displacement map texture*/
+	Texture texture_diffuse;					/**<< The diffuse texture*/
+	Texture texture_specular;					/**<< The specular texture*/
+	Texture texture_normal;						/**<< The normal map texture*/
+	Texture texture_height;						/**<< The displacement map texture*/
 
-	MaterialType _type = MaterialType::PHONG; /**<< The material type*/
+	MaterialType _type = MaterialType::PHONG;	/**<< The material type*/
 
-	float shininess = 0; /**<< The shininess of the material*/
+	float shininess = 0;						/**<< The shininess of the material*/
+	float roughness = 0;						/**<< The roughness of the material (GGX)*/
+	float refractive_index = 0;					/**<< The refractive index*/
 private:
 	std::string _name;
 };
