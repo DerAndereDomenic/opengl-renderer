733ce797948b02fa59aea7d3e5bdb56c9657c69d
Domenic <domeniczingsheim@gmail.com>
2021-02-06 00:48:13 +0100

    Render terrain mesh

--- src/WaterScene/source.cpp
+++ src/WaterScene/source.cpp
@@ -1,21 +1,61 @@
 #include <Renderer/RenderWindow.h>
 #include <IO/KeyManager.h>
+#include <Shader/ShaderManager.h>
+#include <IO/ObjLoader.h>
+#include <Core/GLFunctions.h>
+#include <DataStructure/Scene.h>
 
 int main()
 {
 	uint32_t width = 1280, height = 720;
 
-	Camera camera = Camera::createObject(glm::vec3(0, 0, 0), 90.0f, width / height, 0.01f, 500);
+	Camera camera = Camera::createObject( width / height, 0.01f, 500);
 	RenderWindow window = RenderWindow::createObject(width, height, "WaterScene", &camera);
 	KeyManager::instance()->setup(window);
 
+	ShaderManager::instance()->addShader("Normal");
+
+	std::vector<std::string> names;
+	std::vector<Mesh> meshes;
+	std::vector<Material> material;
+	std::vector<glm::mat4> models;
+
+	names.push_back("Terrain");
+	meshes = ObjLoader::loadObj(RESOURCE_PATH + "terrain.obj");
+	Material terrain = Material::createObject("materialmap", LAMBERT);
+	terrain.ambient = glm::vec3(0.1, 0.1, 0.1);
+	terrain.diffuse = glm::vec3(1, 1, 1);
+	terrain.specular = glm::vec3(1, 1, 1);
+	terrain.shininess = 0.4f * 128.0f;
+	terrain.useTextures = false;
+	material.push_back(terrain);
+	models.push_back(glm::mat4(1));
+
+	Scene scene = Scene::createObject(names, meshes, material, models);
+
+	Light light = Light::createObject(glm::vec3(10, 30, 10), false);
+	light.ambient = glm::vec3(0.1f);
+	light.diffuse = glm::vec3(700.0f);
+	light.specular = glm::vec3(700.0f);
+
+	scene.addLight(&light);
+
 	while (window.isOpen())
 	{
+		GL::clear();
+		ShaderManager::instance()->getShader("Normal").bind();
+		ShaderManager::instance()->getShader("Normal").setMVP(glm::mat4(1), camera.getView(), camera.getProjection());
+		ShaderManager::instance()->getShader("Normal").setVec3("viewPos", camera.getPosition());
+
+		scene.passLights2Shader(ShaderManager::instance()->getShader("Normal"));
+		scene.render(ShaderManager::instance()->getShader("Normal"));
+
 		window.spinOnce();
 	}
 
 	RenderWindow::destroyObject(window);
 	Camera::destroyObject(camera);
+	Scene::destroyObject(scene);
 
 	return 0;
 }
