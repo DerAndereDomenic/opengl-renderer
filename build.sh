2c4530222f7751f659a8ff1c6585d647b3a460b6
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-02-24 21:28:55 +0000

    Add setup option

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ DLOGGER_DIR="lib/DLogger"
 
 first_build=false
 
+for OPTION in "$@"
+do
+    case "${OPTION}"
+    in
+        --setup) ;&
+        -s) first_build=true;;
+    esac
+done
+
 if [ ! -d "$BUILD_DIR" ]; then
 	echo "Creating build directory!";
 	mkdir "$BUILD_DIR";
