158234d492b15d6a617abce0d7d591c9edd12f07
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-03-07 11:23:30 +0100

    Add logger

--- src/GeometryProcessing/source.cpp
+++ src/GeometryProcessing/source.cpp
@@ -1,6 +1,13 @@
+#include <OpenGLRendererConfig.h>
+#include <Core/Platform.h>
+#include <Core/GLFunctions.h>
+#include <DLogger/Logger.h>
 
 int main()
 {
+    LOGGER::setProject("Geometry Processing", std::to_string(OpenGLRenderer_VERSION_MAJOR) + "." + std::to_string(OpenGLRenderer_VERSION_MINOR));
+	LOGGER::start();
     
+    LOGGER::end();
     return 0;
 }
