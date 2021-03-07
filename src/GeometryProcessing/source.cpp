9dbd6f111f1810ecf64525ed8d6d43c3fd8a37bb
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-03-07 11:40:01 +0100

    Create window

--- src/GeometryProcessing/source.cpp
+++ src/GeometryProcessing/source.cpp
@@ -3,11 +3,24 @@
 #include <Core/GLFunctions.h>
 #include <DLogger/Logger.h>
 
+#include <Renderer/RenderWindow.h>
+
 int main()
 {
     LOGGER::setProject("Geometry Processing", std::to_string(OpenGLRenderer_VERSION_MAJOR) + "." + std::to_string(OpenGLRenderer_VERSION_MINOR));
 	LOGGER::start();
     
+    uint32_t width = 1280, height = 720;
+    RenderWindow window = RenderWindow::createObject(width, height, "Geometry");
+
+    while(window.isOpen())
+    {
+        GL::clear();
+
+        window.spinOnce();
+    }
+
+    RenderWindow::destroyObject(window);
     LOGGER::end();
     return 0;
 }
