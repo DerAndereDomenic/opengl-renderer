b9fd7bacdfb632dc758bc213d1dc5b2c34a2e8be
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-03-07 11:41:40 +0100

    Add callbacks

--- src/GeometryProcessing/source.cpp
+++ src/GeometryProcessing/source.cpp
@@ -4,6 +4,8 @@
 #include <DLogger/Logger.h>
 
 #include <Renderer/RenderWindow.h>
+#include <GUI/WindowClose.h>
+#include <GUI/ModeControl.h>
 
 int main()
 {
@@ -12,6 +14,12 @@ int main()
     
     uint32_t width = 1280, height = 720;
     RenderWindow window = RenderWindow::createObject(width, height, "Geometry");
+
+    WindowClose close_callback(&window);
+	ModeControl mode_callback(&window);
+
+	window.registerKeyCallback(GLFW_KEY_ESCAPE, &close_callback);
+	window.registerKeyCallback(GLFW_KEY_LEFT_ALT, &mode_callback);
 
     while(window.isOpen())
     {
