19435f637e385b4c16f23c5369b101951fcada49
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-03-29 16:54:11 +0000

    Implement creation method

--- lib/OpenGL Renderer/src/OpenGLObjects/impl/GBuffer.cpp
+++ lib/OpenGL Renderer/src/OpenGLObjects/impl/GBuffer.cpp
@@ -3,7 +3,20 @@
 GBuffer 
 GBuffer::createObject(const uint32_t& width, const uint32_t& height)
 {
+    GBuffer result;
 
+    result._positionTexture = Texture::createObject(width, height, (float*)NULL, TEXTURE, GL_RGBA16F, GL_RGBA, GL_FLOAT);
+    result._normalTexture = Texture::createObject(width, height, (float*)NULL, TEXTURE, GL_RGBA16F, GL_RGBA, GL_FLOAT);
+    result._albedoSpecTexture = Texture::createObject(width, height, (uint8_t*)NULL, TEXTURE, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE);
+
+    result._fbo = FrameBuffer::createObject(width, height);
+    result._fbo.attachColor(result._positionTexture);
+    result._fbo.attachColor(result._normalTexture);
+    result._fbo.attachColor(result._albedoSpecTexture);
+    result._fbo.attachRenderBuffer();
+    result._fbo.verify();
+
+    return result;
 }
 
 void 
@@ -21,5 +34,5 @@ GBuffer::bind()
 void 
 GBuffer::bindTextures()
 {
-    
+
 }
