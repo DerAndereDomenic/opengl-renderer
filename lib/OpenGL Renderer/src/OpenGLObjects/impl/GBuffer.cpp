6c7aa45e282daf27997b999c92ef0f82fff86111
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-03-29 16:44:28 +0000

    Add gbuffer source

---
+++ lib/OpenGL Renderer/src/OpenGLObjects/impl/GBuffer.cpp
@@ -0,0 +1,25 @@
+#include <OpenGLObjects/GBuffer.h>
+
+GBuffer 
+GBuffer::createObject(const uint32_t& width, const uint32_t& height)
+{
+
+}
+
+void 
+GBuffer::destroyObject(GBuffer& object)
+{
+
+}
+
+void 
+GBuffer::bind()
+{
+
+}
+
+void 
+GBuffer::bindTextures()
+{
+    
+}
