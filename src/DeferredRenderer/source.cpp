b5c05712422555a24e1a13c97c01f15c9a130c09
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-03-29 17:40:26 +0000

    Add Geometry deferred shader

--- src/DeferredRenderer/source.cpp
+++ src/DeferredRenderer/source.cpp
@@ -6,6 +6,7 @@
 #include <DataStructure/MeshHelper.h>
 #include <OpenGLRendererConfig.h>
 #include <Shader/ShaderManager.h>
+#include <OpenGLObjects/GBuffer.h>
 
 int main()
 {
@@ -20,20 +21,25 @@ int main()
     window.registerKeyCallback(GLFW_KEY_ESCAPE, &close_callback);
 
     GL::enableDebugOutput();
+    //GL::updateDrawBuffers(3);
 
     Mesh crate = MeshHelper::cubeMesh(glm::vec4(1));
     crate.create();
     Texture diffuse_texture = Texture::createObject(RESOURCE_PATH + "crate_diffuse.png");
     Texture specuar_texture = Texture::createObject(RESOURCE_PATH + "crate_specular.png");
 
-    ShaderManager::instance()->addShader("basic");
+    ShaderManager::instance()->addShader("GeometryDeferred");
 
     while(window.isOpen())
     {
         GL::clear();
 
-        ShaderManager::instance()->getShader("basic").bind();
-        ShaderManager::instance()->getShader("basic").setMVP(glm::mat4(1), camera.getView(), camera.getProjection());
+        ShaderManager::instance()->getShader("GeometryDeferred").bind();
+        ShaderManager::instance()->getShader("GeometryDeferred").setMVP(glm::mat4(1), camera.getView(), camera.getProjection());
+        ShaderManager::instance()->getShader("GeometryDeferred").setInt("texture_diffuse", 0);
+        ShaderManager::instance()->getShader("GeometryDeferred").setInt("texture_specular", 1);
+        diffuse_texture.bind(0);
+        specuar_texture.bind(1);
         crate.render();
 
         window.spinOnce();
