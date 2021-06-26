0a333da13f11217befb157cf6980085fab185085
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-06-27 00:25:29 +0200

    Add documentation

--- lib/OpenGL Renderer/src/DataStructure/Model.h
+++ lib/OpenGL Renderer/src/DataStructure/Model.h
@@ -13,33 +13,68 @@
 class Model
 {
 	public:
+		/**
+		*	\brief Default constructor
+		*/
 		Model() = default;
 
+		/**
+		*	\brief Create a model
+		*	\param[in] path The path to the file (gltf works best)
+		*	\param[in] calcTangents If tangents (for normal mapping) should be calculated
+		*/
 		Model(const std::string& path, const bool& calcTangents = false);
 
+		/**
+		*	\brief Destructor
+		*/
 		~Model();
 
+		/**
+		*	\brief Renders the model
+		*	\param[in] shader The shader
+		*/
 		void 
 		render(const std::shared_ptr<Shader> shader);
 
 	private:
+		/**
+		*	\brief Load the model
+		* 	\param[in] path The path to the file (gltf works best)
+		*	\param[in] calcTangents If tangents (for normal mapping) should be calculated
+		*/
 		void 
 		loadModel(const std::string& path, const bool& calcTangents);
 
+		/**
+		*	\brief Process a assimp node
+		*	\param[in] node The node to process
+		*	\param[in] scene The scene
+		*	\param[in] calcTangents If tangents (for normal mapping) should be calculated
+		*/
 		void 
 		processNode(aiNode* node, const aiScene *scene, const bool& calcTangents);
 
+		/**
+		*	\brief Processes a mesh
+		*	\param[in] mesh The mesh to be processed
+		*	\param[in] scene The scene
+		*	\param[in] calcTangents If tangents (for normal mapping) should be calculated
+		*	\return A pointer to the mesh
+		*/
 		std::shared_ptr<Mesh> 
 		processMesh(aiMesh* mesh, const aiScene* scene, const bool& calcTangents);
 
+		/**
+		*	\brief Load the mateiral of a scene
+		*	\param[in] scene The scene to process
+		*/
 		void
 		loadMaterials(const aiScene* scene);
 
-		std::vector<std::pair<std::shared_ptr<Mesh>, int32_t>> meshes;
-
-		std::vector<std::shared_ptr<Material>> materials;
-
-		std::string directory;
+		std::vector<std::pair<std::shared_ptr<Mesh>, int32_t>> meshes;		/**<< A vector with meshes and index to the material */
+		std::vector<std::shared_ptr<Material>> materials;					/**<< A vector with materials */
+		std::string directory;												/**<< The model directory */
 };
 
 #endif
