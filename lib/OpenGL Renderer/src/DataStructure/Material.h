3e1301eb1dc5e3c721e1fd656efea2903b659263
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-06-29 23:10:50 +0200

    Add glass properties to materialA

--- lib/OpenGL Renderer/src/DataStructure/Material.h
+++ lib/OpenGL Renderer/src/DataStructure/Material.h
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 #include "OpenGLObjects/Texture.h"
 #include <memory>
+#include <DataStructure/EnvironmentMap.h>
 
 class Shader;
 
@@ -11,7 +12,8 @@ enum MaterialType
 {
 	LAMBERT,
 	PHONG,
-	GGX
+	GGX,
+	GLASS
 };
 
 /**
@@ -43,24 +45,27 @@ public:
 	*/
 	void bind(std::shared_ptr<Shader> shader);
 
-	bool useDiffuseTextures = false;												/**<< Toggle the use of diffuse textures*/
-	bool useSpecularTextures = false;												/**<< Toggle the use of specular textures*/
-	bool useNormalTextures = false;													/**<< Toggle the use of normal textures*/
-	bool useHeightTextures = false;													/**<< Toggle the use of height textures*/
-	glm::vec3 ambient = glm::vec3(0, 0, 0);											/**<< The ambient part of the material*/
-	glm::vec3 diffuse = glm::vec3(0, 0, 0);											/**<< The diffuse part of the material*/
-	glm::vec3 specular = glm::vec3(0, 0, 0);										/**<< The specular part of the material*/
+	bool useDiffuseTextures = false;													/**<< Toggle the use of diffuse textures*/
+	bool useSpecularTextures = false;													/**<< Toggle the use of specular textures*/
+	bool useNormalTextures = false;														/**<< Toggle the use of normal textures*/
+	bool useHeightTextures = false;														/**<< Toggle the use of height textures*/
+	bool dynamic = false;																/**<< If the environment map of this object should be updated*/
 
-	std::shared_ptr<Texture> texture_diffuse = std::make_shared<Texture>();			/**<< The diffuse texture*/
-	std::shared_ptr<Texture> texture_specular = std::make_shared<Texture>();		/**<< The specular texture*/
-	std::shared_ptr<Texture> texture_normal = std::make_shared<Texture>();			/**<< The normal map texture*/
-	std::shared_ptr<Texture> texture_height = std::make_shared<Texture>();			/**<< The displacement map texture*/
+	glm::vec3 ambient = glm::vec3(0, 0, 0);												/**<< The ambient part of the material*/
+	glm::vec3 diffuse = glm::vec3(0, 0, 0);												/**<< The diffuse part of the material*/
+	glm::vec3 specular = glm::vec3(0, 0, 0);											/**<< The specular part of the material*/
 
-	MaterialType _type = MaterialType::PHONG;										/**<< The material type*/
+	std::shared_ptr<Texture> texture_diffuse = std::make_shared<Texture>();				/**<< The diffuse texture*/
+	std::shared_ptr<Texture> texture_specular = std::make_shared<Texture>();			/**<< The specular texture*/
+	std::shared_ptr<Texture> texture_normal = std::make_shared<Texture>();				/**<< The normal map texture*/
+	std::shared_ptr<Texture> texture_height = std::make_shared<Texture>();				/**<< The displacement map texture*/
+	std::shared_ptr<EnvironmentMap> environment = std::make_shared<EnvironmentMap>();	/**<< The environment map */
 
-	float shininess = 0;															/**<< The shininess of the material*/
-	float roughness = 0;															/**<< The roughness of the material (GGX)*/
-	float refractive_index = 1;														/**<< The refractive index*/
+	MaterialType _type = MaterialType::PHONG;											/**<< The material type*/
+
+	float shininess = 0;																/**<< The shininess of the material*/
+	float roughness = 0;																/**<< The roughness of the material (GGX)*/
+	float refractive_index = 1;															/**<< The refractive index*/
 private:
 	std::string _name;
 };
