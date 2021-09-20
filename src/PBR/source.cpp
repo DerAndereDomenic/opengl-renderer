5fbca8fafc11b5d7ab5e099295ea58ace8adbdce
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-09-20 16:41:43 +0200

    add pbr project

---
+++ src/PBR/source.cpp
@@ -0,0 +1,98 @@
+#include <Renderer/RenderWindow.h>
+#include <IO/KeyManager.h>
+#include <Shader/ShaderManager.h>
+#include <IO/ObjLoader.h>
+#include <Core/GLFunctions.h>
+#include <DataStructure/Scene.h>
+#include <GUI/WindowClose.h>
+#include <DLogger/Logger.h>
+#include <DataStructure/MeshHelper.h>
+#include <glm/gtx/transform.hpp>
+
+int main()
+{
+	LOGGER::setProject("PBR", "1.0");
+	LOGGER::start();
+
+	uint32_t width = 1280, height = 720;
+
+	Camera camera = Camera( static_cast<float>(width) / static_cast<float>(height), 0.01f, 500);
+	RenderWindow window = RenderWindow(width, height, "PBR", &camera);
+
+	GL::enableDebugOutput();
+
+	WindowClose close_callback(&window);
+
+	window.registerKeyCallback(GLFW_KEY_ESCAPE, &close_callback);
+
+	ShaderManager::addShader("Normal");
+	ShaderManager::addShader("Post");
+
+	std::vector<std::string> names;
+	std::vector<std::shared_ptr<Mesh>> meshes;
+	std::vector<Material> material;
+	std::vector<glm::mat4> models;
+
+	names.push_back("Sphere");
+	meshes = ObjLoader::loadObj("res/sphere.obj");
+	Material sphere = Material("materialmap", GGX);
+	sphere.ambient = glm::vec3(0.1, 0.0, 0.0);
+	sphere.diffuse = glm::vec3(1, 0, 0);
+	sphere.specular = glm::vec3(1, 0, 0);
+	sphere.roughness = 0.5f;
+
+	material.push_back(sphere);
+	models.push_back(glm::mat4(1));
+	
+
+	Scene scene = Scene(names, meshes, material, models);
+
+	Light light = Light(glm::vec3(10, 30, 10), false);
+	light.ambient = glm::vec3(1.0f);
+	light.diffuse = glm::vec3(700.0f);
+	light.specular = glm::vec3(700.0f);
+
+	scene.addLight(&light);
+
+	FrameBuffer hdr(width, height);
+	hdr.attachHDR();
+	hdr.attachRenderBuffer();
+	hdr.verify();
+
+	FrameBuffer::bindDefault();
+
+	std::shared_ptr<Mesh> screen_quad = MeshHelper::quadMesh(2.0f);
+	screen_quad->create();
+
+	while (window.isOpen())
+	{
+		hdr.bind();
+
+		GL::clear();
+
+		ShaderManager::getShader("Normal")->bind();
+		ShaderManager::getShader("Normal")->setMVP(glm::mat4(1), camera.getView(), camera.getProjection());
+		ShaderManager::getShader("Normal")->setVec3("viewPos", camera.getPosition());
+
+		scene.passLights2Shader(ShaderManager::getShader("Normal"));
+
+		scene.render(ShaderManager::getShader("Normal"));
+
+		hdr.bindDefault();
+		
+		GL::clear();
+
+		ShaderManager::getShader("Post")->bind();
+		ShaderManager::getShader("Post")->setFloat("exposure", 1.0f);
+
+		hdr.getTexture()->bind();
+
+		screen_quad->render();
+
+		window.spinOnce();
+	}
+
+	LOGGER::end();
+
+	return 0;
+}
