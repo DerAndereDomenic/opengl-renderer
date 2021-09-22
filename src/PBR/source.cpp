c20d4ca9635e5afd1fc23d5fab2b532b405f8718
Domenic Zingsheim <domeniczingsheim@gmail.com>
2021-09-22 15:18:39 +0200

    In front of main building of the university

--- src/PBR/source.cpp
+++ src/PBR/source.cpp
@@ -40,25 +40,23 @@ int main()
 	std::vector<glm::mat4> models;
 
 	names.push_back("Sphere");
-	meshes = ObjLoader::loadObj("res/sphere.obj", true);
+	meshes = ObjLoader::loadObj("res/uv_sphere.obj", true);
 	Material sphere = Material("materialmap", GGX);
-	sphere.useDiffuseTextures = false;
-	sphere.useMetallicTextures = false;
-	sphere.useNormalTextures = false;
-	sphere.useRoughnessTextures = false;
+	sphere.useDiffuseTextures = true;
+	sphere.useMetallicTextures = true;
+	sphere.useNormalTextures = true;
+	sphere.useRoughnessTextures = true;
 	sphere.useIBLTextures = true;
 
-	//sphere.texture_diffuse = std::make_shared<Texture>("res/rustediron2_basecolor.png");
-	//sphere.texture_metallic = std::make_shared<Texture>("res/rustediron2_metallic.png");
-	//sphere.texture_normal = std::make_shared<Texture>("res/rustediron2_normal.png");
-	//sphere.texture_roughness = std::make_shared<Texture>("res/rustediron2_roughness.png");
-	//sphere.texture_irradiance = std::make_shared<EnvironmentMap>(glm::vec3(0), 32, 32);
-	//sphere.texture_prefilter = std::make_shared<EnvironmentMap>(glm::vec3(0), 128, 128);
-	sphere.ambient = glm::vec3(0.1 ,0.1 ,0.1);
-	sphere.diffuse = glm::vec3(1, 1, 1);
-	sphere.specular = glm::vec3(1, 1, 1);
-	sphere.roughness = 0.4f;
-	sphere.metallic = 0.8f;
+	sphere.texture_diffuse = std::make_shared<Texture>("res/rustediron2_basecolor.png");
+	sphere.texture_metallic = std::make_shared<Texture>("res/rustediron2_metallic.png");
+	sphere.texture_normal = std::make_shared<Texture>("res/rustediron2_normal.png");
+	sphere.texture_roughness = std::make_shared<Texture>("res/rustediron2_roughness.png");
+	//sphere.ambient = glm::vec3(0.1 ,0.1 ,0.1);
+	//sphere.diffuse = glm::vec3(1, 1, 1);
+	//sphere.specular = glm::vec3(1, 1, 1);
+	//sphere.roughness = 1.0f;
+	//sphere.metallic = 0.0f;
 
 	material.push_back(sphere);
 	models.push_back(glm::mat4(1));
@@ -99,12 +97,17 @@ int main()
 	capture_buffer.verify();
 	FrameBuffer::bindDefault();
 
-	std::shared_ptr<Texture> skybox = std::make_shared<Texture>("res/skybox/", faces);
+	glDepthFunc(GL_LEQUAL);  
+
 	std::shared_ptr<Texture> hdr_equirect = std::make_shared<Texture>("res/main_building.hdr", true);
+	std::shared_ptr<Texture> hdr_cubemap = Texture::createTexture(512, 512, (float*)nullptr, CUBEMAP, GL_RGBA16F, GL_RGBA, GL_FLOAT);
+	std::shared_ptr<EnvironmentMap> dummy_cm = std::make_shared<EnvironmentMap>(glm::vec3(0));
+	dummy_cm->setCubeMap(hdr_equirect);
 
 	std::shared_ptr<EnvironmentMap> skybox_map = std::make_shared<EnvironmentMap>(glm::vec3(0));
-	skybox_map->setCubeMap(skybox);
-	glDepthFunc(GL_LEQUAL);  
+	skybox_map->setCubeMap(hdr_cubemap);
+	skybox_map->setSkybox(dummy_cm);
+	skybox_map->renderTo(nullptr, ShaderManager::getShader("Normal"), ShaderManager::getShader("ERtoCube"));
 
 	EnvironmentMap irradiance_map(glm::vec3(0));
 	irradiance_map.setSkybox(skybox_map);
